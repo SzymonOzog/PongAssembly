189fa483806db9dfd0f570dba5ee7ea84a12adfd
Szymon Ożóg <58388001+SzymonOzog@users.noreply.github.com>
2024-03-08 08:22:04 +0100

    window refresh and buffer drawing

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
 nasm -f win64 -gcv8 pong.asm 
-gcc -o pong.exe pong.obj
+gcc -o pong.exe pong.obj -lgdi32
 if "%1" == "run" (pong.exe)
--- pong.asm
+++ pong.asm
@@ -1,16 +1,28 @@
 bits 64
 default rel
 
+WIDTH equ 800
+HEIGHT equ 600
+WM_PAINT equ 0x000F
+
+
 segment .data
-    format db "%d", 0xd, 0xa, 0
+    format db "value %d", 0xd, 0xa, 0
+    error db "error %d", 0xd, 0xa, 0
     window_name db "pong window", 0
     style dw 0x3
     hbr_background dw 0x5
     dw_style dd 0xcf0000
+    handle dq 1
 
 segment .bss
     hInstance resb 8
     win_class resb 80
+    buffer resb 4*WIDTH*HEIGHT
+    rect resb 16
+    paint_struct resb 72
+    bitmap_info resb 44
+    msg resb 48
 
 segment .text
     global main
@@ -21,6 +33,18 @@ segment .text
     extern DefWindowProcA
     extern GetLastError
     extern ShowWindow
+    extern GetWindowRect
+    extern BeginPaint
+    extern EndPaint
+    extern SetDIBitsToDevice 
+    extern GetWindowRect
+    extern TranslateMessage
+    extern DispatchMessageA
+    extern IsWindow
+    extern GetMessageA
+    extern InvalidateRect
+    extern UpdateWindow
+    
 
 main:
     push    rbp
@@ -28,7 +52,25 @@ main:
     sub     rsp, 32
 
     call    create_window
+    call  clear_buffer
+
+.MAIN_LOOP:
+    call redraw
+    lea rcx, [msg]
+    mov rdx, qword[handle]
+    mov r8, 0
+    mov r9, 0
+    call GetMessageA
+    lea rcx, [msg]
+    call TranslateMessage
+    lea rcx, [msg]
+    call DispatchMessageA
+    mov rcx, qword[handle]
+    call IsWindow
+    cmp rax, 0
+    jne .MAIN_LOOP
     xor     rax, rax
+    call    ExitProcess
 
 create_window:
     push    rbp
@@ -38,7 +80,7 @@ create_window:
     ; wndclassA
     mov dword[win_class], 80
     mov dword[win_class+4], 0x3
-    lea rax, [DefWindowProcA]
+    lea rax, [window_proc]
     mov qword[win_class+8], rax 
     mov dword[win_class+16], 0
     mov dword[win_class+20], 0
@@ -56,10 +98,9 @@ create_window:
     call RegisterClassExA
 
     mov ecx, 0
-    lea rdx, [window_name]
     mov rdx, qword[win_class+56]
     mov r8, qword[win_class+64]
-    mov r9, dw_style
+    mov r9, qword[dw_style]
 
     mov qword[rsp+4*8], 200
     mov qword[rsp+5*8], 200
@@ -72,12 +113,134 @@ create_window:
     mov qword[rsp+11*8], 0
     call CreateWindowExA
 
-    mov    rcx, rax
+    cmp rax, 0
+    jne .NO_ERROR
+    call GetLastError
+    lea rcx, [error]
+    mov rdx, rax
+    call printf
+    call ExitProcess
+    .NO_ERROR:
+    mov qword[handle], rax
+    mov    rcx, qword[handle] 
     mov    rdx, 10
     call   ShowWindow
+
+    add rsp, 32 + 8*8
     leave
     ret
 
 window_proc:
     push rbp
     mov rbp, rsp
+    sub rsp, 32 + 4*8
+    
+    mov qword[rsp+4*8], rcx
+    mov qword[rsp+5*8], rdx
+    mov qword[rsp+6*8], r8
+    mov qword[rsp+7*8], r9
+
+    cmp qword[rsp+5*8], WM_PAINT
+    jne .CONTINUE
+    call draw_buffer
+.CONTINUE:
+    mov rcx, qword[rsp+4*8]
+    mov rdx, qword[rsp+5*8]
+    mov r8, qword[rsp+6*8]
+    mov r9, qword[rsp+7*8]
+    call DefWindowProcA
+    leave 
+    ret
+clear_buffer:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 32
+    mov r10, 0
+    lea r11, [buffer]
+.LOOP:
+    mov dword[r11], 0xFF0000
+    add r10, 1
+    add r11, 4
+    cmp r10, WIDTH*HEIGHT
+    jl .LOOP
+    leave
+    ret
+
+redraw:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 32
+
+    mov qword[rect], 0
+    mov qword[rect+4], 0
+    mov qword[rect+8], WIDTH
+    mov qword[rect+12], HEIGHT
+
+    mov rcx, qword[handle]
+    lea rdx, [rect]
+    mov r8, 0
+    call InvalidateRect
+    leave
+    ret
+
+draw_buffer:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 32 + 8
+
+    mov rcx, qword[handle]
+    lea rdx, [rect]
+    call GetWindowRect
+    
+    mov qword[paint_struct], 0
+    mov dword[paint_struct+8], 1
+    mov rax, qword[rect]
+    mov qword[paint_struct+12], rax
+    mov rax, qword[rect+8]
+    mov qword[paint_struct+20], rax 
+    mov dword[paint_struct+44], 0
+    mov dword[paint_struct+48], 0
+    
+    mov rcx, qword[handle]
+    lea rdx, [paint_struct]
+    call BeginPaint
+
+    mov qword[rsp+32], rax
+    
+    mov dword[bitmap_info], 40
+    mov dword[bitmap_info+4], WIDTH 
+    mov dword[bitmap_info+8], HEIGHT 
+    mov word[bitmap_info+12], 1
+    mov word[bitmap_info+14], 32
+    mov dword[bitmap_info+16], 0
+    mov dword[bitmap_info+20], 0
+    mov qword[bitmap_info+24], 0
+    mov qword[bitmap_info+28], 0
+    mov qword[bitmap_info+32], 0
+    mov dword[bitmap_info+36], 0
+    mov dword[bitmap_info+40], 0
+
+    mov rcx, qword[rsp+32] 
+    mov rdx, 0
+    mov r8, 0
+    mov r9, WIDTH 
+    sub rsp, 8*8
+    mov qword[rsp+4*8], HEIGHT
+    mov qword[rsp+5*8], 0
+    mov qword[rsp+6*8], 0
+    mov qword[rsp+7*8], 0
+    mov qword[rsp+8*8], HEIGHT
+    lea rax, [buffer]
+    mov qword[rsp+9*8], rax
+    lea rax, [bitmap_info]
+    mov qword[rsp+10*8], rax
+    mov qword[rsp+11*8], 0
+    call SetDIBitsToDevice 
+
+    mov rcx, qword[handle]
+    lea rdx, [paint_struct]
+    call EndPaint
+
+    leave 
+    ret
+
